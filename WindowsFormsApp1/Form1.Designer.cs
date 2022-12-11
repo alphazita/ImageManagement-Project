2de4d7030fadc46f9559c532203ba2cd43e15793
Anastasiaa <Anastasiaa@ANASTASIA>
2022-12-12 01:47:53 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonstart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonstart
+            // 
+            this.buttonstart.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonstart.Location = new System.Drawing.Point(299, 71);
+            this.buttonstart.Name = "buttonstart";
+            this.buttonstart.Size = new System.Drawing.Size(141, 23);
+            this.buttonstart.TabIndex = 0;
+            this.buttonstart.Text = "ΕΚΚΙΝΗΣΗ";
+            this.buttonstart.UseVisualStyleBackColor = false;
+            this.buttonstart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(158, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(424, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.Red;
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete.Location = new System.Drawing.Point(671, 18);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(103, 37);
+            this.delete.TabIndex = 3;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonstart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonstart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var listImages = Directory.GetFiles(@"C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\", "*.jpg");
+            List<string> savedImages = new List<string>();
+            //check if we have empty folder
+            if (listImages.Length == 0)
+            {
+                List<string> imageUrls = new List<string>
+                {
+                "https://www.taxydromiki.com/sites/default/files/images/20160323_pireas.jpg",
+                "https://www.taxydromiki.com/sites/default/files/styles/flexslider_full/public/hq_gt.jpg",
+                "https://www.taxydromiki.com/sites/default/files/images/eblokotel2_0.jpg",
+                "https://www.taxydromiki.com/sites/default/files/styles/flexslider_full/public/slide-2_0.jpg",
+                "https://www.taxydromiki.com/sites/default/files/styles/flexslider_full/public/slide-1.jpg",
+                "https://www.taxydromiki.com/sites/default/files/styles/flexslider_full/public/slide-3.jpg"
+                };
+
+                
+
+                // Create an instance of the WebClient class
+
+                WebClient webClient = new WebClient();
+                int classLevelCounter = 0;
+
+
+                //to download the images in parallel to save time 
+                foreach (string url in imageUrls)
+                //Parallel.ForEach(imageUrls, url =>
+                {
+                    try
+                    {
+                        // Download the image from the URL
+                        byte[] imageBytes = webClient.DownloadData(url);
+                        // Save the image as a JPG
+                        using (var stream = new MemoryStream(imageBytes))
+                        using (var image = Image.FromStream(stream))
+                        {
+                            image.Save("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\" + Path.GetFileNameWithoutExtension(url) + ".jpg", ImageFormat.Jpeg);
+                            savedImages.Add("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\" + Path.GetFileNameWithoutExtension(url) + ".jpg");
+                        }
+
+                        // Set up the download progress event handler
+                        webClient.DownloadProgressChanged += (webclient, progressEventArgs) =>
+                        {
+                            // Update the progress bar with the download progress
+                            progressBar1.Value = progressEventArgs.ProgressPercentage;
+                        };
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle any errors that occurred while downloading the image
+                        Console.WriteLine("Error downloading image from URL: " + url);
+                        Console.WriteLine(ex.Message);
+                        System.Windows.Forms.Application.Exit();
+                    }
+                }//);
+            }
+
+            //if is empty upload the saved images
+            if (savedImages.Count == 0)
+            {
+                foreach (string file in Directory.EnumerateFiles("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\", "*.jpg"))
+                {
+                    savedImages.Add(file);
+                }
+            }
+
+            //Disable the button if all images are downloaded
+            if (savedImages.Count==6)
+            {
+                buttonstart.Enabled = false;
+            }
+
+            int thumbWidth = 200;
+            int thumbHeight = 200;
+            //Create the thumbnails 
+            Parallel.ForEach(savedImages, image =>
+            {
+                try
+                {
+                    Image ima = new Bitmap(image);
+                    Image imageThumbnail = ima.GetThumbnailImage(thumbWidth, thumbHeight, null, new IntPtr());
+
+                    System.Windows.Forms.Button thumbnailButton = new System.Windows.Forms.Button();
+                    thumbnailButton.Image = imageThumbnail;
+
+                    // When the user clicks on the thumbnail button, open the full-size image in a scrollable PictureBox
+                    thumbnailButton.Click += (sr, er) =>
+                    {
+                        PictureBox fullSizeImage = new PictureBox();
+                        fullSizeImage.Image = ima;
+                        fullSizeImage.SizeMode = PictureBoxSizeMode.AutoSize;
+                        fullSizeImage.Show();
+                    };
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            });
+
+        }
+        //delete all files from folder
+        private void delete_Click_1(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\");
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
