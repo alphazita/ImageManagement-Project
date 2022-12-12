78f7e3246fef13a87ec0410ab2867d36663b7d75
Anastasiaa <Anastasiaa@ANASTASIA>
2022-12-12 09:44:03 +0200

    Changes for thumbnail

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,12 +11,16 @@ using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Aspose.Slides;
+using Microsoft.SharePoint.Client;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+using Image = System.Drawing.Image;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
         public Form1()
         {
@@ -42,9 +46,6 @@ namespace WindowsFormsApp1
                 "https://www.taxydromiki.com/sites/default/files/styles/flexslider_full/public/slide-1.jpg",
                 "https://www.taxydromiki.com/sites/default/files/styles/flexslider_full/public/slide-3.jpg"
                 };
-
-                
-
                 // Create an instance of the WebClient class
 
                 WebClient webClient = new WebClient();
@@ -58,28 +59,32 @@ namespace WindowsFormsApp1
                     try
                     {
                         // Download the image from the URL
-                        byte[] imageBytes = webClient.DownloadData(url);
-                        // Save the image as a JPG
-                        using (var stream = new MemoryStream(imageBytes))
-                        using (var image = Image.FromStream(stream))
-                        {
-                            image.Save("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\" + Path.GetFileNameWithoutExtension(url) + ".jpg", ImageFormat.Jpeg);
-                            savedImages.Add("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\" + Path.GetFileNameWithoutExtension(url) + ".jpg");
-                        }
+                        string name = Path.GetFileNameWithoutExtension(url) + ".jpg";
+                        if (!listImages.Contains(name)){
+                            byte[] imageBytes = webClient.DownloadData(url);
+                            // Save the image as a JPG
+                            using (var stream = new MemoryStream(imageBytes))
+                            using (var image = Image.FromStream(stream))
+                            {
+                                image.Save("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\" + Path.GetFileNameWithoutExtension(url) + ".jpg", ImageFormat.Jpeg);
+                                savedImages.Add("C:\\Users\\Anastasiaa\\Desktop\\GenikiTaxidromiki\\" + Path.GetFileNameWithoutExtension(url) + ".jpg");
+                            }
 
-                        // Set up the download progress event handler
-                        webClient.DownloadProgressChanged += (webclient, progressEventArgs) =>
-                        {
-                            // Update the progress bar with the download progress
-                            progressBar1.Value = progressEventArgs.ProgressPercentage;
-                        };
+                            // Set up the download progress event handler
+                            webClient.DownloadProgressChanged += (webclient, progressEventArgs) =>
+                            {
+                                // Update the progress bar with the download progress
+                                progressBar1.Value = progressEventArgs.ProgressPercentage;
+                            };
+                        }
+                        
                     }
                     catch (Exception ex)
                     {
                         // Handle any errors that occurred while downloading the image
                         Console.WriteLine("Error downloading image from URL: " + url);
                         Console.WriteLine(ex.Message);
-                        System.Windows.Forms.Application.Exit();
+                        //System.Windows.Forms.Application.Exit();
                     }
                 }//);
             }
@@ -94,33 +99,45 @@ namespace WindowsFormsApp1
             }
 
             //Disable the button if all images are downloaded
-            if (savedImages.Count==6)
-            {
+            //if (savedImages.Count==6)
+            //{
                 buttonstart.Enabled = false;
-            }
+            //}
 
             int thumbWidth = 200;
             int thumbHeight = 200;
+            int i = 0;
             //Create the thumbnails 
-            Parallel.ForEach(savedImages, image =>
+            foreach (string im  in savedImages)
+            //Parallel.ForEach(savedImages, image =>
             {
                 try
                 {
-                    Image ima = new Bitmap(image);
+                    i = i + 50;
+                    Image ima = new Bitmap(im);
                     Image imageThumbnail = ima.GetThumbnailImage(thumbWidth, thumbHeight, null, new IntPtr());
-
+                    //Placeholder ph = new Placeholder();
                     System.Windows.Forms.Button thumbnailButton = new System.Windows.Forms.Button();
                     thumbnailButton.Image = imageThumbnail;
-
+                    thumbnailButton.Name = im;
+                    thumbnailButton.AutoSize = true;
+                    thumbnailButton.Location = new System.Drawing.Point(i,50);
+                    thumbnailButton.Size = new Size(130, 130);
                     // When the user clicks on the thumbnail button, open the full-size image in a scrollable PictureBox
                     thumbnailButton.Click += (sr, er) =>
                     {
+                        Panel MyPanel = new Panel();
+                        MyPanel.Dock = DockStyle.Fill;
                         PictureBox fullSizeImage = new PictureBox();
                         fullSizeImage.Image = ima;
                         fullSizeImage.SizeMode = PictureBoxSizeMode.AutoSize;
+                        MyPanel.Controls.Add(fullSizeImage);
+                        MyPanel.AutoScroll = true;
+                        this.Controls.Add(MyPanel);
                         fullSizeImage.Show();
+                        
                     };
-
+                    this.Controls.Add(thumbnailButton);
 
                 }
                 catch (Exception ex)
@@ -128,7 +145,7 @@ namespace WindowsFormsApp1
                     Console.WriteLine(ex.Message);
                 }
 
-            });
+            }
 
         }
         //delete all files from folder
